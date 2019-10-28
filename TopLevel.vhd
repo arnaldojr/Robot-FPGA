db86ecc4c6c163693adcc3650784ab778509a870
Arnaldo Alves Viana Junior <arnaldo_viana@yahoo.com.br>
2019-10-28 12:33:11 -0300

    Update TopLevel.vhd

--- TopLevel.vhd
+++ TopLevel.vhd
@@ -24,7 +24,10 @@ entity TopLevel is
 	port(
 		CLOCK_50 	: in STD_LOGIC;
 		SW				: in std_logic_vector(9 downto 0);
-		LEDR			: out STD_LOGIC_VECTOR(5 downto 0)
+		KEY			: in std_logic_vector(1 downto 0);
+		LEDR			: out std_logic_vector(1 downto 0);
+		GPIO_0		: out STD_LOGIC_VECTOR(5 downto 0)
+		
 		);
 end entity;
 
@@ -48,6 +51,14 @@ component MotorDC is
 	);
 end component;
 
+component Bumper is
+	port ( 
+			b:   in  STD_LOGIC_VECTOR(1 downto 0);
+			q:   out STD_LOGIC_VECTOR(1 downto 0)
+	);
+end component;
+
+
 --------------
 -- signals
 --------------
@@ -61,15 +72,19 @@ begin
 S0: MotorDC port map (	CLOCK_IN => CLOCK_50, 
 								DIRECAO => SW(4),
 								DUTY(3 DOWNTO 0) => SW(3 DOWNTO 0),
-								DIRECAO_OUT(1 DOWNTO 0) => LEDR(1 DOWNTO 0),
-								PWM_OUT => LEDR(2));
+								DIRECAO_OUT(1 DOWNTO 0) => GPIO_0(1 DOWNTO 0),
+								PWM_OUT => GPIO_0(2)
+							);
 								
 S1: MotorDC port map (	CLOCK_IN => CLOCK_50, 
 								DIRECAO => SW(9),
 								DUTY(3 DOWNTO 0) => SW(8 DOWNTO 5),
-								DIRECAO_OUT(1 DOWNTO 0) => LEDR(4 DOWNTO 3),
-								PWM_OUT => LEDR(5));
+								DIRECAO_OUT(1 DOWNTO 0) => GPIO_0(4 DOWNTO 3),
+								PWM_OUT => GPIO_0(5)
+							);
 
-
+S2: Bumper port map (	b => KEY,
+								q => LEDR
+							);
 
 end rtl;
